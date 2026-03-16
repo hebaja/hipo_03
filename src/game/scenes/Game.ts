231af3e5bb08f003163c6af61698db30defaf483
hebatist <hebaja@yahoo.com.br>
2026-03-16 13:58:26 -0300

    base project done

---
+++ src/game/scenes/Game.ts
@@ -0,0 +1,20 @@
+import { Scene } from 'phaser';
+
+export class Game extends Scene
+{
+    constructor ()
+    {
+        super('Game');
+    }
+
+    preload ()
+    {
+        this.load.setPath('assets');
+        
+    }
+
+    create ()
+    {
+             
+    }
+}
